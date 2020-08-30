f44a9c0fbf77613e8f8e2df0ac13edf6dece35c2
akachukwu <akachi_benji@yahoo.com>
2020-08-30 10:26:36 +0100

    added new files

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,105 @@
-import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+import React, { useRef, useState } from "react";
+import {
+  IonApp,
+  IonHeader,
+  IonContent,
+  IonToolbar,
+  IonTitle,
+  IonGrid,
+  IonItem,
+  IonRow,
+  IonCol,
+  IonLabel,
+  IonInput,
+} from "@ionic/react";
+
+import BmiControls from "./components/BmiControls";
+import BmiResult from "./components/BmiResult";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route path="/home" component={Home} exact={true} />
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  const [result, setResult] = useState<number>();
 
+  //this is because we are using typescript.
+  //with javascript you dont need the <HTML...>after Ref
+  const weightInputRef = useRef<HTMLIonInputElement>(null);
+  const heightInputRef = useRef<HTMLIonInputElement>(null);
+
+  const calculateBmi = () => {
+    //? is a tenary expression to check if the current object
+    //exists.(I.e if we have linked the ref to our html)
+    //! tells typescript that you are sure everything is fine
+    const enteredWeight = weightInputRef.current!.value;
+    const enteredHeight = heightInputRef.current!.value;
+
+    if (
+      !enteredHeight ||
+      !enteredWeight ||
+      +enteredWeight <= 0 ||
+      +enteredHeight <= 0
+    ) {
+      return;
+    }
+    // the + just means that anything that is returned from there is of type number
+    const bmi = +enteredWeight / (+enteredHeight * +enteredHeight);
+
+    setResult(bmi);
+  };
+
+  const resetInputs = () => {
+    weightInputRef.current!.value = "";
+    heightInputRef.current!.value = "";
+  };
+
+  return (
+    <IonApp>
+      <IonHeader>
+        <IonToolbar color="primary">
+          <IonTitle>BMI Calculator</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel position="floating">Your Height</IonLabel>
+                <IonInput type="number" ref={heightInputRef}></IonInput>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel position="floating">Your Weight</IonLabel>
+                <IonInput type="number" ref={weightInputRef}></IonInput>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <BmiControls onCalculate={calculateBmi} onReset={resetInputs} />
+          {result && <BmiResult result={result} />}
+        </IonGrid>
+      </IonContent>
+      <h2>This works</h2>
+    </IonApp>
+  );
+};
 export default App;
---
+++ src/components/BmiControls.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { IonRow, IonButton, IonIcon, IonCol } from "@ionic/react";
+import { calculatorOutline, refreshOutline } from "ionicons/icons";
+
+const BmiControls: React.FC<{
+  onCalculate: () => void;
+  onReset: () => void;
+}> = (props) => {
+  ///we are telling typescript here that we are recieving a props
+  //of type function.
+
+  return (
+    <IonRow>
+      <IonCol className="ion-text-left">
+        <IonButton onClick={props.onCalculate}>
+          <IonIcon slot="start" icon={calculatorOutline} />
+          Calculate
+        </IonButton>
+      </IonCol>
+      <IonCol className="ion-text-right">
+        <IonButton onClick={props.onReset}>
+          <IonIcon slot="start" icon={refreshOutline} />
+          Reset
+        </IonButton>
+      </IonCol>
+    </IonRow>
+  );
+};
+
+export default BmiControls;
