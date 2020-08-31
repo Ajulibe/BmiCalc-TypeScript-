094e321db04eed01f5e57ccefa9889f6f06161b7
akachukwu <akachi_benji@yahoo.com>
2020-08-31 04:53:10 +0100

    added more files

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,12 @@ import {
   IonCol,
   IonLabel,
   IonInput,
+  IonAlert,
 } from "@ionic/react";
 
 import BmiControls from "./components/BmiControls";
 import BmiResult from "./components/BmiResult";
+import InputControl from "./components/InputControl";
 
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
@@ -37,6 +39,7 @@ import "./theme/variables.css";
 
 const App: React.FC = () => {
   const [result, setResult] = useState<number>();
+  const [error, setError] = useState<string>();
 
   //this is because we are using typescript.
   //with javascript you dont need the <HTML...>after Ref
@@ -56,6 +59,7 @@ const App: React.FC = () => {
       +enteredWeight <= 0 ||
       +enteredHeight <= 0
     ) {
+      setError("Please enter a valid (non-negatve) input number");
       return;
     }
     // the + just means that anything that is returned from there is of type number
@@ -69,37 +73,61 @@ const App: React.FC = () => {
     heightInputRef.current!.value = "";
   };
 
+  const clearError = () => {
+    setError("");
+  };
+
   return (
-    <IonApp>
-      <IonHeader>
-        <IonToolbar color="primary">
-          <IonTitle>BMI Calculator</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        <IonGrid>
-          <IonRow>
-            <IonCol>
-              <IonItem>
-                <IonLabel position="floating">Your Height</IonLabel>
-                <IonInput type="number" ref={heightInputRef}></IonInput>
-              </IonItem>
-            </IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol>
-              <IonItem>
-                <IonLabel position="floating">Your Weight</IonLabel>
-                <IonInput type="number" ref={weightInputRef}></IonInput>
-              </IonItem>
-            </IonCol>
-          </IonRow>
-          <BmiControls onCalculate={calculateBmi} onReset={resetInputs} />
-          {result && <BmiResult result={result} />}
-        </IonGrid>
-      </IonContent>
-      <h2>This works</h2>
-    </IonApp>
+    <>
+      {/* !!converts error to a boolean from a string. Alternatively
+      we would have assigned two different states to error and message
+      Look ath teh documentation for how they did it*/}
+      <IonAlert
+        isOpen={!!error}
+        message={error}
+        buttons={[
+          {
+            text: "Okay",
+            handler: clearError,
+          },
+        ]}
+      />
+      <IonApp>
+        <IonHeader>
+          <IonToolbar color="primary">
+            <IonTitle>BMI Calculator</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          <IonGrid>
+            <IonRow>
+              <IonCol>
+                <InputControl selectedValue="mkg" />
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol>
+                <IonItem>
+                  <IonLabel position="floating">Your Height</IonLabel>
+                  <IonInput type="number" ref={heightInputRef}></IonInput>
+                </IonItem>
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol>
+                <IonItem>
+                  <IonLabel position="floating">Your Weight</IonLabel>
+                  <IonInput type="number" ref={weightInputRef}></IonInput>
+                </IonItem>
+              </IonCol>
+            </IonRow>
+            <BmiControls onCalculate={calculateBmi} onReset={resetInputs} />
+            {result && <BmiResult result={result} />}
+          </IonGrid>
+        </IonContent>
+        <h2>This works</h2>
+      </IonApp>
+    </>
   );
 };
 export default App;
--- src/components/BmiResult.tsx
+++ src/components/BmiResult.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { IonRow, IonCol, IonCard, IonCardContent } from "@ionic/react";
 
-const BmiResult: React.FC<{ result: number | string }> = ({ result }) => {
+const BmiResult: React.FC<{ result: number }> = ({ result }) => {
   return (
     <IonRow>
       <IonCol>
         <IonCard>
-          <IonCardContent>
-            <h2>{result}</h2>
+          <IonCardContent className="ion-text-center">
+            <h2>Your Body-mass-Index</h2>
+            <h3>{result.toFixed(2)}</h3>
           </IonCardContent>
         </IonCard>
       </IonCol>
